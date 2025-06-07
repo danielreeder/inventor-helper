2732da1fd61f180f13218f3b89802c9c26efdae8
danielreeder <101375530+danielreeder@users.noreply.github.com>
2025-06-06 20:29:29 -0700

    added support for all file types

--- app.py
+++ app.py
@@ -4,91 +4,105 @@ from parts import *
 from PIL import Image, ImageTk
 from functools import partial
 
-app = Tk(screenName='Inventor Helper', baseName='Inventor Helper', className='Inventor Helper')
-app.title("Inventor Helper")
-app.geometry("960x1080")
+class App():
+    def __init__(self):
+        self.app = Tk(screenName='Inventor Helper', baseName='Inventor Helper', className='Inventor Helper')
+        self.num_parts = get_num_files_open()
+        self.part_cards = []
+        self.part_cards_container = PanedWindow(self.app, orient='vertical', bg='lightblue')
 
-menu = Menu(app)
-app.config(menu=menu)
-file_menu = Menu(menu)
-menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open Parts", command=open_parts)
-file_menu.add_command(label="Close Parts", command=close_parts)
-file_menu.add_command(label="Exit", command=app.quit)
-parts, thumbnails = get_parts()
-i = 0 
-j = 0
-part_cards = []
+    def main_loop(self):
+        current_num_parts = get_num_files_open()
+        pw_msg = PanedWindow(orient='vertical', bg='lightblue')
+        no_parts_msg = Message(pw_msg, text="No parts open. Please open parts from the File menu.", width=500)
+        if current_num_parts == 0:
+            pw_msg.add(no_parts_msg)
+            pw_msg.grid(row=0, column=0, padx=10, pady=10)
+        if current_num_parts != self.num_parts:
+            self.num_parts = current_num_parts
+            
+            for part_card in self.part_cards:
+                part_card.destroy()
+            self.part_cards = []
 
-for part, thumbnail in zip(parts, thumbnails):
-    part_name = part.split('\\')[-1]  # Get the file name only
-    part_name = part_name.replace('.ipt', '')  # Remove the file extension
+            # Remove the "no parts" message if it exists
+            for widget in self.app.grid_slaves():
+                if isinstance(widget, PanedWindow):
+                    children = widget.winfo_children()
+                    if any(isinstance(child, Message) and "No parts open" in str(child.cget("text")) for child in children):
+                        widget.destroy()
 
-    pw = PanedWindow(orient='vertical', bg='lightblue')
-    msg = Message(app, text=f"{part_name}", width=500)
+            self.setup_part_cards()
+            print(f"Updated parts: {len(self.part_cards)}")
 
-    img = Image.open(thumbnail)
-    tk_img = ImageTk.PhotoImage(img)
-    img_label = Label(pw, image=tk_img, anchor='w')
-    img_label.image = tk_img  # Keep a reference to avoid garbage collection
-    btn = Button(pw, text="Close Part", command=partial(close_part, part_name))
-    
-    pw.add(msg)
-    pw.add(img_label)
-    pw.add(btn)
-    pw.grid(row=j, column=i, padx=10, pady=10)
-    if i == 1:
-        i = 0
-        j += 1
-    else:
-        i += 1
-    part_cards.append(pw)
-orig_num_parts = get_num_parts_open()
+        self.app.after(100, self.main_loop)  # Check for updates every second
 
-while True:
-    current_num_parts = get_num_parts_open()
-    pw_msg = PanedWindow(orient='vertical', bg='lightblue')
-    no_parts_msg = Message(pw_msg, text="No parts open. Please open parts from the File menu.", width=500)
-    if current_num_parts == 0:
-        pw_msg.add(no_parts_msg)
-        pw_msg.grid(row=0, column=0, padx=10, pady=10)
-    if current_num_parts != orig_num_parts:
-        parts, thumbnails = get_parts()
-        orig_num_parts = current_num_parts
-        print(f"Updated parts: {len(parts)}")
-        for part_card in part_cards:
-            part_card.destroy()
-        # Remove the "no parts" message if it exists
-        for widget in app.grid_slaves():
-            if isinstance(widget, PanedWindow):
-                children = widget.winfo_children()
-                if any(isinstance(child, Message) and "No parts open" in str(child.cget("text")) for child in children):
-                    widget.destroy()
+    def run(self):
+        wc.Dispatch("Inventor.Application")
+        self.setup()
+        self.app.after(100, self.main_loop)  # Check for updates every second
+        self.app.mainloop()
+
+    def setup(self):
+        self.app.title("Inventor Helper")
+        self.app.geometry("960x1080")
+
+        file_menu = self.add_menu(self.app, "File")
+        file_menu.add_command(label="Open File", command=open_file)
+        file_menu.add_command(label="Open Folder", command=open_folder)
+        file_menu.add_command(label="Close Files", command=close_files)
+        file_menu.add_command(label="Exit", command=self.app.quit)
+        self.setup_part_cards()
+        self.part_cards_container.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
+
+    def add_menu(self, parent, label):
+        menu = Menu(parent)
+        self.app.config(menu=menu)
+        file_menu = Menu(menu)
+        menu.add_cascade(label=label, menu=file_menu)
+        return file_menu
+
+    def setup_part_cards(self):
+        parts, thumbnails = get_open_files()
         i = 0 
         j = 0
-        part_cards = []
 
         for part, thumbnail in zip(parts, thumbnails):
-            part = part.split('\\')[-1]  # Get the part name only
-            part = part.replace('.ipt', '')  # Remove the file extension
-            pw = PanedWindow(orient='vertical', bg='lightblue')
-            msg = Message(app, text=part, width=500)
+            part_name = part.split('\\')[-1]  # Get the file name only
+            part_name = part_name.split('.')[0]  # Remove the file extension
+
+            pw1 = PanedWindow(orient='vertical', bg='lightblue')
+            pw2 = PanedWindow(pw1, orient='horizontal', bg='lightblue')
+            pw2_1 = PanedWindow(pw2, orient='vertical', bg='lightblue')
+
+            msg = Message(self.app, text=f"{part_name}", width=500)
 
             img = Image.open(thumbnail)
             tk_img = ImageTk.PhotoImage(img)
-            img_label = Label(pw, image=tk_img, anchor='w')
+            img_label = Label(pw2, image=tk_img, anchor='w')
             img_label.image = tk_img  # Keep a reference to avoid garbage collection
+            btn = Button(pw2_1, text="Close Part", command=partial(close_file, part_name))
+            btn.config(width=10, height=2, bg='red', fg='white', font=('Arial', 12, 'bold'))
 
-            btn = Button(pw, text="Close Part", command=partial(close_part, part))
-            pw.add(msg)
-            pw.add(img_label)
-            pw.add(btn)
+            pw2_1.add(btn)
+
+            pw2.add(img_label)
+            pw2.add(pw2_1)
             
-            pw.grid(row=j, column=i, padx=10, pady=10)
-            if i == 1:
+            pw1.add(msg)
+            pw1.add(pw2)
+
+            row = j
+            col = i
+            self.part_cards_container.add(pw1)
+
+            pw1.grid(row=row, column=col, padx=10, pady=10)
+
+            if i == 2:
                 i = 0
                 j += 1
             else:
                 i += 1
-            part_cards.append(pw)
-    app.update()
+            self.part_cards.append(pw1)
+        
+App().run()
--- parts.py
+++ parts.py
@@ -1,23 +1,22 @@
 import win32com.client as wc
 from tkinter import Tk
-from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askdirectory, askopenfilename
 import os
 import time
 
-def close_part(part_name):
+def close_file(part_name):
     inventor = wc.Dispatch("Inventor.Application")
-    part_name = part_name + '.ipt'  # Ensure the part name is in lowercase and has the correct extension
-
+    part_name = part_name.split('.')[0]
     for doc in inventor.Documents:
-        part_name_doc = doc.FullFileName.split('\\')[-1]
-        print(part_name_doc)
+        part_name_doc = doc.FullFileName.split('\\')[-1].split('.')[0]  # Get the part name without extension
+        print(part_name_doc, part_name)
         if part_name_doc.lower() == part_name.lower():
             doc.Close(False)
             print(f"Closed {part_name}.")
             return
     print(f"Part {part_name} is not currently open.")
 
-def open_parts():
+def open_folder():
     inventor = wc.Dispatch("Inventor.Application")
 
     folder = askdirectory(initialdir=os.path.expanduser("~\\Desktop\\Inventor Projects"))
@@ -26,22 +25,28 @@ def open_parts():
         to_open = folder + '/' + ipt
         inventor.Documents.open(to_open, True)
         print(f"Opened {to_open}.")
+    
+def open_file():
+    inventor = wc.Dispatch("Inventor.Application")
+    file_path = askopenfilename(initialdir=os.path.expanduser("~\\Desktop\\Inventor Projects"))
+    if file_path:
+        inventor.Documents.Open(file_path, True)
+        print(f"Opened {file_path}.")
+    else:
+        print("No file selected.")
 
-def close_parts():
+def close_files():
     inventor = wc.Dispatch("Inventor.Application")
     inventor.Documents.CloseAll(False)
     print("Closed all documents.")
 
-def get_parts():
+def get_open_files():
     inventor = wc.Dispatch("Inventor.Application")
-    parts = []
+    open_files = []
     thumbnails = []
     
     for doc in inventor.Documents.VisibleDocuments:
-        if not doc.FullFileName.lower().endswith('.ipt'):
-            continue
-        
-        parts.append(doc.FullFileName)
+        open_files.append(doc.FullFileName)
 
         width = 256
         height = 256
@@ -49,20 +54,19 @@ def get_parts():
         doc.Activate()
 
         # Build a unique thumbnail path for each part
-        part_name = os.path.splitext(os.path.basename(doc.FullFileName))[0]
-        thumbnail_path = os.path.expanduser(f"~\\Desktop\\Inventor Projects\\{part_name}_thumbnail.bmp")
+        file_name = os.path.splitext(os.path.basename(doc.FullFileName))[0]
+        thumbnail_path = os.path.expanduser(f"~\\Desktop\\Inventor Projects\\{file_name}_thumbnail.bmp")
         
         # Save the active view as a bitmap
         inventor.ActiveView.SaveAsBitmap(thumbnail_path, width, height)
         print(f"Thumbnail image saved as {thumbnail_path}")
 
         thumbnails.append(thumbnail_path)
-    return parts, thumbnails
+    return open_files, thumbnails
 
-def get_num_parts_open():
+def get_num_files_open():
     inventor = wc.Dispatch("Inventor.Application")
-    count = 0
-    for doc in inventor.Documents:
-        if doc.FullFileName.lower().endswith('.ipt'):
-            count += 1
-    return len(inventor.Documents)
+    return len(inventor.Documents.VisibleDocuments)
+
+inventor = wc.Dispatch("Inventor.Application")
+print(inventor.Documents.VisibleDocuments.count)
